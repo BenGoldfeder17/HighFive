c056be0beb511832564baa006e0514b01eeb1340
blake-tropper <blakeethantropper@gmail.com>
2025-03-19 15:47:36 -0600

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,1 +1,11 @@
+import os
+import torch
+import torchvision
+from torch.utils.data import random_split
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+data_dir  = '/kaggle/input/garbage-classification/Garbage classification/Garbage classification'
 
+classes = os.listdir(data_dir)
+print(classes)
