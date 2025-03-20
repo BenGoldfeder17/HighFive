f9c724374d53a387aa6db29852f29737a3bb62f0
blake-tropper <blakeethantropper@gmail.com>
2025-03-20 16:51:30 -0600

    Update ai.py

--- ai.py
+++ ai.py
@@ -10,3 +10,10 @@ data_dir  = '/kaggle/input/garbage-classification/Garbage classification/Garbage
 classes = os.listdir(data_dir)
 print(classes)
 pip install imx500-converter[tf]
+
+pip install tensorflow datasets huggingface_hub opencv-python-headless
+from datasets import load_dataset
+
+# Load dataset from Hugging Face
+dataset = load_dataset("viola77data/recycling-dataset")
+print(dataset)
